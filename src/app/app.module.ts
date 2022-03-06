4743823f7513beedc44539497bccefc2808ad532
TumalaSaihoshini <100579244+TumalaSaihoshini@users.noreply.github.com>
2022-03-06 09:46:31 +0530

    Add files via upload
    added contact component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,14 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { UserSignupComponent } from './user-signup/user-signup.component';
 import { HomeComponent } from './home/home.component';
+import { NgoRegisterComponent } from './ngo-register/ngo-register.component';
+import { NgoLoginComponent } from './ngo-login/ngo-login.component';
 import { NgoVolunteerListComponent } from './ngo-volunteer-list/ngo-volunteer-list.component';
 
 import { AboutComponent } from './about/about.component';
 import { FaqComponent } from './faq/faq.component';
 import { TrainingsComponent } from './trainings/trainings.component';
-import { NgoComponent } from './ngo/ngo.component';
-import { NgoDetailsComponent } from './ngo-details/ngo-details.component';
+import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
@@ -26,13 +27,14 @@ import { ProfileComponent } from './profile/profile.component';
     UserLoginComponent,
     UserSignupComponent,
     HomeComponent,
-    NgoComponent,
-    NgoDetailsComponent,
+    NgoRegisterComponent,
+    NgoLoginComponent,
     NgoVolunteerListComponent,
 
     AboutComponent,
     FaqComponent,
     TrainingsComponent,
+    ContactComponent,
     ProfileComponent,
   ],
   imports: [
